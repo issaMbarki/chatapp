8c2fc2b81e042ea156b365051510fb1d84b67571
issamSeo <mbarkiseomaniak@gmail.com>
2023-08-16 19:40:05 +0100

    create room

---
+++ client/src/components/PrivateRoom/CreateRoomDialog.jsx
@@ -0,0 +1,125 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { useState } from "react";
+import LockIcon from "@mui/icons-material/Lock";
+import PeopleIcon from "@mui/icons-material/People";
+import InfoIcon from "@mui/icons-material/Info";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import Grid from "@mui/material/Grid";
+import { ClickAwayListener, Tooltip } from "@mui/material";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function CreateRoomDialog({ open, setOpen }) {
+  const [userCount, setUserCount] = useState(2);
+  const [roomType, setRoomType] = useState("open");
+  const [Titleopen, setTitleOpen] = React.useState(false);
+  React.useEffect(() => {
+    console.log(roomType);
+  }, [roomType]);
+
+  const handleAgree = () => {
+    // Perform actions with userCount and roomType
+    setOpen(false);
+  };
+  const handleTooltipClose = () => {
+    setTitleOpen(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setTitleOpen(true);
+  };
+  return (
+    <div>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={() => {
+          setOpen(false);
+        }}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle display="flex" alignItems="center" gap="1rem">
+          Create new private room
+          <ClickAwayListener onClickAway={handleTooltipClose}> 
+          <Tooltip
+            PopperProps={{
+              disablePortal: true,
+            }}
+            
+            onClose={handleTooltipClose}
+            open={Titleopen}
+            disableFocusListener
+            disableHoverListener
+            disableTouchListener
+            title={
+                <>
+                  Participants: Specify the maximum number of users allowed in the room.
+                  <br />
+                  Room type: An open room allows anyone with room code to join, while a locked room requires validation from you.
+                  <br />
+                 You can always change these settings after creating the room.
+                </>
+              } >
+            <InfoIcon onClick={handleTooltipOpen}>Click</InfoIcon>
+          </Tooltip>
+          </ClickAwayListener>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            <Grid container spacing={3}>
+              <Grid item display="flex" alignItems="center">
+                <PeopleIcon />
+                <FormControl sx={{ m: 1, minWidth: 150 }}>
+                  <InputLabel id="participants">Participants</InputLabel>
+                  <Select
+                    labelId="participants"
+                    label="Participants"
+                    value={userCount}
+                    onChange={(e) => setUserCount(parseInt(e.target.value))}
+                  >
+                    <MenuItem value="2">2</MenuItem>
+                    <MenuItem value="3">3</MenuItem>
+                    <MenuItem value="4">4</MenuItem>
+                    <MenuItem value="5">5</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item display="flex" alignItems="center">
+                <LockIcon />
+                <FormControl sx={{ m: 1, minWidth: 120 }}>
+                  <InputLabel id="room-type">Room type</InputLabel>
+                  <Select
+                    labelId="room-type"
+                    label="Room type"
+                    value={roomType}
+                    onChange={(e) => setRoomType(e.target.value)}
+                  >
+                    <MenuItem value="open">Open</MenuItem>
+                    <MenuItem value="locked">Locked</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Cancel</Button>
+          <Button onClick={handleAgree}>Create</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- client/src/pages/PrivateRoom.jsx
+++ client/src/pages/PrivateRoom.jsx
@@ -1,44 +1,55 @@
 import { Button, Grid, Paper, TextField, Typography } from "@mui/material";
-import home from '../assets/home.svg'
+import home from "../assets/home.svg";
+import { useState } from "react";
+import CreateRoomDialog from "../components/PrivateRoom/CreateRoomDialog";
 
 export default function PrivateRoom() {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <Grid
-      container
-      spacing={0}
-     
-      sx={{
-        direction:"column",
-        alignItems:"center",
-        justifyContent:"center",
-        backgroundImage: `url(${home})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "contain",
-        backgroundPosition: "left",
-        minHeight:'90vh',
-      }}
-    >
-      
-      <Grid item>
-        <Paper elevation={3} sx={{ padding: '2rem', maxWidth: '400px' }}>
-          <Typography variant="h6" gutterBottom>
-            Private Room
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            Enter the room code below to join an existing room, or create a new room.
-          </Typography>
-          <TextField label="Room Code" variant="outlined" fullWidth />
-          <Button variant="contained" color="primary" sx={{ mt: '1rem', mr:'1rem'}}>
-            Join Room
-          </Button>
-          <Button variant="outlined" color="primary" sx={{ mt: '1rem', }}>
-            Create New Room
-          </Button>
-        </Paper>
+        container
+        spacing={0}
+        sx={{
+          direction: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundImage: `url(${home})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "contain",
+          backgroundPosition: "left",
+          minHeight: "90vh",
+        }}
+      >
+        <Grid item>
+          <Paper elevation={3} sx={{ padding: "2rem", maxWidth: "400px" }}>
+            <Typography variant="h6" gutterBottom>
+              Private Room
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Enter the room code below to join an existing room, or create a
+              new room.
+            </Typography>
+            <TextField label="Room Code" variant="outlined" fullWidth />
+            <Button
+              variant="contained"
+              color="primary"
+              sx={{ mt: "1rem", mr: "1rem" }}
+            >
+              Join Room
+            </Button>
+            <Button
+              variant="outlined"
+              color="primary"
+              sx={{ mt: "1rem" }}
+              onClick={() => setOpen(true)}
+            >
+              Create New Room
+            </Button>
+          </Paper>
+        </Grid>
       </Grid>
-    </Grid>
-  
+      <CreateRoomDialog open={open} setOpen={setOpen} />
     </>
   );
 }
