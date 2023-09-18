a494a5cf6b2cde9d60769c7fe967208d570a8ff9
issamSeo <mbarkiseomaniak@gmail.com>
2023-09-18 16:16:34 +0100

    better ui

--- client/src/components/loading/RoomsLoading.jsx
+++ client/src/components/loading/RoomsLoading.jsx
@@ -1,32 +1,32 @@
-import { Grid, Skeleton, Stack, useTheme } from '@mui/material'
+import { Grid, Skeleton, Stack, useTheme } from "@mui/material";
 
-export const RoomsLoading = ({contentHeight}) => {
-    const theme = useTheme();
+export const RoomsLoading = ({ contentHeight }) => {
+  const theme = useTheme();
   return (
     <Grid item xs={12} sm={4}>
-    <Stack
-      spacing={1}
-      sx={{
-        height: contentHeight,
-        overflow: "auto",
-        borderRadius: 1,
-        boxShadow: 1,
-        backgroundColor: theme.palette.listBackGround.main,
-        px: 2,
-        py: 2,
-      }}
-    >
-      <Skeleton variant="rounded" height="20%" animation="wave" />
-      <Skeleton variant="rounded" animation="wave" />
-      <Skeleton variant="rounded" height="20%" animation="wave" />
-      <Skeleton variant="rounded" animation="wave" />
-      <Skeleton variant="rounded" height="20%" animation="wave" />
-      <Skeleton variant="rounded" animation="wave" />
-      <Skeleton variant="rounded" height="20%" animation="wave" />
-      <Skeleton variant="rounded" animation="wave" />
-      <Skeleton variant="rounded" height="20%" animation="wave" />
-      <Skeleton variant="rounded" animation="wave" />
-    </Stack>
-  </Grid>
-  )
-}
+      <Stack
+        spacing={1}
+        sx={{
+          height: contentHeight,
+          overflow: "auto",
+          borderRadius: 1,
+          boxShadow: 1,
+          backgroundColor: theme.palette.listBackGround.main,
+          px: 2,
+          py: 2,
+        }}
+      >
+        <Skeleton variant="rounded" height="20%" animation="pulse" />
+        <Skeleton variant="rounded" animation="wave" />
+        <Skeleton variant="rounded" height="20%" animation="pulse" />
+        <Skeleton variant="rounded" animation="wave" />
+        <Skeleton variant="rounded" height="20%" animation="pulse" />
+        <Skeleton variant="rounded" animation="wave" />
+        <Skeleton variant="rounded" height="20%" animation="pulse" />
+        <Skeleton variant="rounded" animation="wave" />
+        <Skeleton variant="rounded" height="20%" animation="pulse" />
+        <Skeleton variant="rounded" animation="wave" />
+      </Stack>
+    </Grid>
+  );
+};
