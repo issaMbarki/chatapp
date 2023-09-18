1ac88d6460267082c95fbb333bb5f23cfcfaf23d
issamSeo <mbarkiseomaniak@gmail.com>
2023-09-18 16:50:41 +0100

    socket connection

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -10,7 +10,7 @@ const SocketContextProvider = ({ children }) => {
   const{_id:userId}=useContext(UserContext)
   useEffect(() => {
     // Connect to the Socket.IO server when the component mounts
-    socketRef.current = io("http://localhost:4000");
+    socketRef.current = io(process.env.REACT_APP_SERVER_URI);
     // Set the connection flag when the connection is established
     socketRef.current.on("connect", () => {
       setIsSocketConnected(true)
