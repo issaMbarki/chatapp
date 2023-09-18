59e9ea32f338f330c8e18aef32a3032f8f66bc7a
issamSeo <mbarkiseomaniak@gmail.com>
2023-09-18 16:54:36 +0100

    no conv

--- client/src/components/private-rooms/NoConv.jsx
+++ client/src/components/private-rooms/NoConv.jsx
@@ -1,24 +1,28 @@
-import {  Grid, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import startConv from "../../assets/start_conv.svg";
 import startConvDark from "../../assets/dark-mode/start_conv_dark.svg";
 import { useTheme } from "@emotion/react";
 
 export const NoConv = () => {
-  const theme=useTheme()
-  const currentTheme=theme.palette.mode
+  const theme = useTheme();
+  const currentTheme = theme.palette.mode;
   return (
     <Grid
       item
       sm={8}
       sx={{
-        display: { xs:"none",sm:"flex" },
+        display: { xs: "none", sm: "flex" },
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         gap: 3,
       }}
     >
-      <img src={currentTheme==="dark"?startConvDark:startConv} alt="start conversation" style={{ width: "60%" }} />
+      <img
+        src={currentTheme === "dark" ? startConvDark : startConv}
+        alt="start conversation"
+        style={{ width: "60%" }}
+      />
       <Typography variant="h6" sx={{ textAlign: "center" }}>
         Welcome! Choose a room to begin your conversation.
       </Typography>
