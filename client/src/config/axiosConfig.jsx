09b06c3a73d58853d4a888293240a2411cf2bf35
issamSeo <mbarkiseomaniak@gmail.com>
2023-09-18 16:53:23 +0100

    Axios config

--- client/src/config/axiosConfig.jsx
+++ client/src/config/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: process.env.REACT_APP_AXIOS_PHONE_URI, 
-    baseURL: process.env.REACT_APP_AXIOS_URI,
+    baseURL: process.env.REACT_APP_SERVER_URI,
     withCredentials: true 
   });
 export default api;
